93529e047f9ef0e9ebe1867282f11c380166320c
DanielaVenturi <danielaventuri14@gmail.com>
2026-09-15 13:45:44 -0300

    Adicionado método ligar

--- src/Maquina.java
+++ src/Maquina.java
@@ -1,3 +1,43 @@
 public class Maquina {
+          private boolean maquinaLigada;
+          private Estado estado;
+//        private boolean maquinaDesligada;
+//        private boolean iniciarLavagem;
+//        private boolean pausarlavagem;
+//        private boolean retomarLavagem;
+//        private boolean iniciarCentrifuga;
+//        private boolean abrirTampa;
+        private boolean tampaFechada;
+
+    public Maquina(){
+        this.maquinaLigada = false;
+        this.tampaFechada = false;
+        this.estado = Estado.DESLIGADA;
+    }
+    public void ligar(){
+        if (estado == Estado.DESLIGADA){
+            estado = Estado.LIGADA;
+            System.out.println("maquina Ligada");
+        }else {
+            System.out.println("maquina ja esta ligada");
+        }
+    }
+
+
+
+    public enum Estado {
+        DESLIGADA,
+        LIGADA,
+        LAVANDO,
+        PAUSADA,
+        CENTRIFUGANDO,
+        FINALIZADA
+    }
+
+
 
 }
+
+
+
+
